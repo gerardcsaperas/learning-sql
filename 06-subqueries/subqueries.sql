bc53132d5e9dc7656a3f4885c53fd3141d2a0798
Gerard C. Saperas <gcsaperas@gmail.com>
2020-08-12 13:56:53 +0200

    Subqueries

--- 06-subqueries/subqueries.sql
+++ 06-subqueries/subqueries.sql
@@ -1,1 +1,26 @@
+# SUBQUERIES
+# QUERY THE RESULTS OF A PREVIOUS QUERY
 
+/*
+Example 1: Select only the users that have published posts
+*/
+SELECT * FROM users
+WHERE ID IN (
+    SELECT UserID FROM posts
+);
+
+/*
+Example 2: Select users that have
+posts with the title '%Japan%'
+*/
+SELECT FirstName, LastName FROM USERS
+WHERE ID IN (
+    SELECT UserID FROM posts
+    WHERE PostTitle LIKE "%Japan%"
+);
+
+SELECT * FROM posts
+WHERE CategoryID IN (
+    SELECT ID FROM categories
+    WHERE CategoryName LIKE 'Nutrition'
+);
