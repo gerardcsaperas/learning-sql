2c0b84bd8b4e92cdf4fab111a8379b0d006dcdc9
Gerard C. Saperas <gcsaperas@gmail.com>
2020-08-12 11:43:05 +0200

    database design + tables creation for a blog

---
+++ 03-design/blog.sql
@@ -0,0 +1,28 @@
+# CREATE USERS TALBE
+CREATE TABLE users(
+    ID int(255) PRIMARY KEY AUTO_INCREMENT,
+    FirstName varchar(255) NOT NULL,
+    LastName varchar(255) NOT NULL,
+    Email varchar(255) NOT NULL UNIQUE,
+    Password varchar(255) NOT NULL,
+    RegisterDate date NOT NULL
+)ENGINE=InnoDB;
+
+# CREATE CATEGORIES TABLE
+CREATE TABLE categories(
+    ID int(255) PRIMARY KEY AUTO_INCREMENT,
+    CategoryName varchar(255) NOT NULL
+)ENGINE=InnoDB;
+
+# CREATE POSTS TABLE
+# References IDs from users and categories with FOREIGN KEY
+CREATE TABLE posts(
+    ID int(255) PRIMARY KEY AUTO_INCREMENT,
+    UserID int(255) NOT NULL,
+    CategoryID int(255) NOT NULL,
+    PostTitle varchar(255) NOT NULL,
+    PostDesc MEDIUMTEXT,
+    PostDate date NOT NULL,
+    FOREIGN KEY (UserID) REFERENCES users(ID),
+    FOREIGN KEY (CategoryID) REFERENCES categories(ID),
+)ENGINE=InnoDB;
