f424e1b848974fa56de9f90cdb4eaa23d8a31fb3
Gerard C. Saperas <gcsaperas@gmail.com>
2020-08-12 11:58:20 +0200

    Arithmetic Operations in MySQL

---
+++ 04-dml/insert.sql
@@ -0,0 +1,14 @@
+# INSERT NEW REGISTERS
+
+INSERT INTO users (FirstName, LastName, Email, Password, RegisterDate)
+VALUE('Mònica', 'Velázquez Silvestre', 'monica@gmail.com', 'contrasenya', '2020-08-12');
+
+INSERT INTO users (FirstName, LastName, Email, Password, RegisterDate)
+VALUE('Albert', 'Serra Santos', 'albert@gmail.com', 'contrasenya', '2020-08-12');
+
+INSERT INTO users (FirstName, LastName, Email, Password, RegisterDate)
+VALUE('Mr', 'Test Test', 'test@gmail.com', 'contrasenya', '2020-08-12');
+
+# CONSULT REGISTERS
+SELECT * FROM users;
+
